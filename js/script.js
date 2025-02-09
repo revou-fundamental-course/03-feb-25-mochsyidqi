a690b9aa901933219f8c85f4a4cec2074ffa009d
Moch Syidqi Imam Tholabi <moch.tholabi@toyota.co.id>
2025-02-09 23:23:22 +0700

    Rev2

--- js/script.js
+++ js/script.js
@@ -5,24 +5,20 @@
 // Call the greet function
 //greet();
 
-document.getElementById('reset').addEventListener('click', function() {
-    document.getElementById('suhu-celcius').value = '';
-    document.getElementById('suhu-fahrenheit').value = '';
+document.getElementById("reset").addEventListener("click", function () {
+  document.getElementById("suhu-celcius").value = "";
+  document.getElementById("suhu-fahrenheit").value = "";
 });
 
-document.getElementById('konversi').addEventListener('click', function() {
-    var celciusValue = document.getElementById('suhu-celcius').value;
-    var fahrenheitValue = document.getElementById('suhu-fahrenheit').value;
+document.getElementById("konversi").addEventListener("click", function () {
+  var celciusValue = document.getElementById("suhu-celcius").value;
+  var fahrenheitValue = document.getElementById("suhu-fahrenheit").value;
 
-    if (celciusValue !== '') {
-        fahrenheitValue = parseFloat(celciusValue)+ 32;
-        document.getElementById('suhu-fahrenheit').value = fahrenheitValue;
-    } else if (fahrenheitValue !== '') {
-        celciusValue = parseFloat(fahrenheitValue) - 32;
-        document.getElementById('suhu-celcius').value = celciusValue;
-    }
+  if (celciusValue !== "") {
+    fahrenheitValue = parseFloat(celciusValue) + 32;
+    document.getElementById("suhu-fahrenheit").value = fahrenheitValue;
+  } else if (fahrenheitValue !== "") {
+    celciusValue = parseFloat(fahrenheitValue) - 32;
+    document.getElementById("suhu-celcius").value = celciusValue;
+  }
 });
-
-
-  
-  
